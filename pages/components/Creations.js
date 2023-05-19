ff3b422e173d201f2b22c80973170fa5f0486eab
mattleesounds <mattleesounds@gmail.com>
2023-05-19 10:25:42 -0500

    u

--- pages/components/Creations.js
+++ pages/components/Creations.js
@@ -6,46 +6,47 @@ import Card from './Card'
 const Creations = () => {
   return (
     /* Title */
-    <section className="mt-[125px] ">
+    <section className="mt-[110px] ">
       <div className="text-center text-gray-light font-coda">
-        <h1 className="text-3xl font-bold ">Creations</h1>
+        <h1 className="text-3xl font-bold ">Recent Creations</h1>
       </div>
       {/* Cards */}
       <div >
         <div className="flex flex-col md:flex-row justify-center">
         <Card
+            image="/polp-icon.png"
+            title="POLP (Private Beta)"
+            descr="Proof of Listenership Platform is a music streaming app with integrated NFTs and community management tools. Currently in private beta, so this links to the landing page."
+            link="https://polp.app/"
+            tech="Next.js, Typescript, Tailwind CSS, AWS, Ethereum"
+            alt="Image for POLP proof of concept"
+          />
+          <Card
+            image="/aimusic.png"
+            title="AI Music Promo"
+            descr="An application using OpenAI's API to generate promotional instagram posts for music releases."
+            link="https://aimusicpromo.com/"
+            tech="OpenAI API, Next.js, Tailwind CSS, Amazon Cognito"
+            alt="Image for POLP"
+          />
+        
+        </div>
+        <div className="flex flex-col md:flex-row mb-16 justify-center">      
+        <Card
             image="/color-chords.png"
             title="Color Chords"
-            descr="Push the button to randomly generate a color, which then is used to play a chord based on the color with Tone.js."
+            descr="Push the button to randomly generate a color. A chord whose harmonic content is based on the color will play."
             link="https://www.colorchords.xyz/"
-            tech="Tone.js"
+            tech="Tone.js, Tailwind CSS"
             alt="Image for Color Chords"
           />
           <Card
             image="/buddha.png"
             title="Buddha Says"
             descr="An OpenAI-based web application that allows you to speak to the buddha. Namaste."
-            link="https://github.com/mattleesounds/buddhasays"
+            link="https://buddhasays.vercel.app/"
             tech="Next.js, OpenAI API"
             alt="Image of Buddha"
-          />
-        </div>
-        <div className="flex flex-col md:flex-row mb-16 justify-center">      
-          <Card
-            image="/polp-icon.png"
-            title="POLP Proof of Concept"
-            descr="Stream music Prove listenership after listening to 30 seconds of each song by signing a message with your MetaMask wallet"
-            link="https://github.com/Proof-of-Listenership-Protocol/MVPv1"
-            tech="Ethers.js, Supabase"
-            alt="Image for POLP proof of concept"
-          />
-          <Card
-            image="/POLP2.png"
-            title="POLP"
-            descr="An application to stream user-generated audio content that incentivizes active music discovery"
-            link="https://polp.app"
-            tech="Svelte.js"
-            alt="Image for POLP"
           />
         </div>
       </div>
